c56c04729fc180848abb39e6c2e7a4875e52881a
Mezz <lars.gustafson@gmail.com>
2021-07-13 09:27:17 +0200

    Extracted code to methods and more comments

--- arduino/Pump/Pump.ino
+++ arduino/Pump/Pump.ino
@@ -15,6 +15,9 @@ int time = 0;
 // Resets the arduino
 void(* reset) (void) = 0;
 
+void setTime(int);
+void printRemainingTime();
+
 void setup(){
   pinMode(dir, OUTPUT);
   pinMode(pwm, OUTPUT);
@@ -40,22 +43,33 @@ void loop(){
 void serialEvent() {
  char command = (char) Serial.read();
  
- if (command >= '0' && command <= '9') { // Set pump time.
-  time = (command - '0') * 10 * 60;
-
- } else if (command == 'r' || command == 'R') { // Check remaining time
-  if (time > 0) {
-    char text[50];
-    sprintf(text,"Remaining time: %ds", time / 10);
-    Serial.println(text);
-  } else {
-    Serial.println("Stoped");
-  }
-
+ if (command >= '0' && command <= '9') {
+  setTime(command - '0');
+ } else if (command == 'r' || command == 'R') {
+  printRemainingTime();
  } else if (command == 'q') {
     reset();
  } else { // Unknown command
   Serial.println("Illegal command"); 
  }
+}
 
+void setTime(int minutes) {
+    time = minutes * 10 * 60;
+    char text[50];
+    sprintf(text,"Set time to %d minutes", minutes);
+    Serial.println(text);
+}
+  
+void printRemainingTime() {
+  if (time > 0) {
+    int minutes = time / 10 / 60;
+    int seconds = time / 10 % 60;
+    
+    char text[50];
+    sprintf(text,"Remaining time: %dm %ds", minutes, seconds);
+    Serial.println(text);
+  } else {
+    Serial.println("Stoped");
+  }
 }
