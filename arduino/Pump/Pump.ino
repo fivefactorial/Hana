1c6889b757cd26caf9847057499594a0671cac95
Mezz <lars.gustafson@gmail.com>
2021-07-19 23:14:48 +0200

    Update adruino
    Support for longer commands
    Now sets time by typing the number of seconds
    Min time = 0
    Max time = 999 seconds
    
    Changed command r to ? - it returns remaining time
    Changed command q to reset - resets the arduino
    Added command stop - does the same as 0

--- arduino/Pump/Pump.ino
+++ arduino/Pump/Pump.ino
@@ -12,8 +12,16 @@ int pwm = 3;
 // Time remaining in deciseconds (100 milliseconds)
 int time = 0;
 
+const int bufferSize = 50;
+char buffer[bufferSize];
+int pointer = 0;
+
 // Resets the arduino
 void(* reset) (void) = 0;
+
+void resetBuffer();
+void handleCommand();
+int toNumer();
 
 void setTime(int);
 void printRemainingTime();
@@ -22,6 +30,7 @@ void setup(){
   pinMode(dir, OUTPUT);
   pinMode(pwm, OUTPUT);
   
+  resetBuffer();
   Serial.begin(9600);
   while (!Serial);
   
@@ -41,24 +50,77 @@ void loop(){
 }
 
 void serialEvent() {
- char command = (char) Serial.read();
- 
- if (command >= '0' && command <= '9') {
-  setTime(command - '0');
- } else if (command == 'r' || command == 'R') {
-  printRemainingTime();
- } else if (command == 'q') {
-    reset();
- } else { // Unknown command
-  Serial.println("Illegal command"); 
- }
+  while (Serial.available()) {
+    if (pointer == bufferSize-1) {
+      resetBuffer();
+      Serial.println("To long command. Buffer overflow!");
+    }    
+    char next = (char) Serial.read();
+    if (next == '\n') {
+      handleCommand();
+      resetBuffer();
+    } else {
+     buffer[pointer++] = next;
+    }
+  }
 }
 
-void setTime(int minutes) {
-    time = minutes * 10 * 60;
-    char text[50];
-    sprintf(text,"Set time to %d minutes", minutes);
-    Serial.println(text);
+void resetBuffer() {
+  pointer = 0;
+  for (int x = 0; x < bufferSize; x++) {
+    buffer[x] = 0;
+  }
+}
+
+void handleCommand() {
+  int number = toNumber();
+
+  if (number >= 0) {
+    setTime(number) ;
+  } else {
+    String command = buffer;
+    if (command.equals("?")) {
+      printRemainingTime();
+    } else if (command.equalsIgnoreCase("reset")) {
+      reset(); 
+    } else if (command.equalsIgnoreCase("stop")) {
+      setTime(0);
+    } else if (pointer != 0) {
+      Serial.println("Illegal command!");
+    }
+  }
+
+}
+
+int toNumber() {
+  if (pointer == 0) return -2; // Empty string
+  if (pointer > 3) return -3; // String is too long to parse
+  
+  int number = 0;
+  for (int x = 0; x < pointer; x++) {
+    if (buffer[x] >= '0' && buffer[x] <= '9') {
+      number = number * 10 + buffer[x] - '0';    
+    } else {
+      return -1; // Contains non numeric character
+    }
+  }  
+
+  return number; // Parsed number in [0, 999]
+}
+
+void setTime(int seconds) {
+    time = seconds * 10;
+    
+    if (seconds == 0) {
+      Serial.println("Stopping pump!");
+    } else {
+      int minutes = seconds / 60;
+      int secondsRemaining = seconds % 60;
+    
+      char text[50];
+      sprintf(text,"New time: %dm %ds", minutes, secondsRemaining);
+      Serial.println(text);
+    }
 }
   
 void printRemainingTime() {
